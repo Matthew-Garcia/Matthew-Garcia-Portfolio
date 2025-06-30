66cc7a83d5633bf58f2f987843153891f50258b8
Matthew Garcia <127278603+Matthew-Garcia@users.noreply.github.com>
2025-06-30 15:07:32 -0500

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -15,11 +15,11 @@ const Footer = ({isDarkMode}) => {
       </div>
 
     <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
-        <p>© 2025 GreatStack. All rights reserved.</p>
+        <p>© 2025 Matthew Garcia. All rights reserved.</p>
         <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
-            <li><a target='_blank' href="https://instagram.com/greatstackdev">GitHub</a></li>
-            <li><a target='_blank' href="https://instagram.com/greatstackdev">LinkedIn</a></li>
-            <li><a target='_blank' href="https://instagram.com/greatstackdev">Twitter</a></li>
+            <li><a target='_blank' href="https://github.com/Matthew-Garcia">GitHub</a></li>
+            <li><a target='_blank' href="https://www.linkedin.com/in/matthew-garcia-165634195/">LinkedIn</a></li>
+            <li><a target='_blank' href="https://app.joinhandshake.com/profiles/r36cj8">HandShake</a></li>
         </ul>
     </div>
 
