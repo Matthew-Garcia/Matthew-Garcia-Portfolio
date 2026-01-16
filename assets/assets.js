46b803da4cd2f13489dee1f47c1b67357541dbee
Matthew Garcia <127278603+Matthew-Garcia@users.noreply.github.com>
2026-01-16 14:43:56 -0600

    Update assets.js

--- assets/assets.js
+++ assets/assets.js
@@ -101,7 +101,7 @@ export const workData = [
     title: 'NASA Lunabotics Autonomous Rover',
     description: 'Embedded Systems, ROS2, micro-ROS, Robotics',
     bgImage: '/lunabotics-rover.png',
-    link: 'https://github.com/Matthew-Garcia/uhcl-lunar-hawks-lunabotics'
+    link: 'https://github.com/Matthew-Garci/uhcl-lunar-hawks-lunabotics'
     },
     {
         title: 'SCARA Robot Arm',
@@ -138,6 +138,7 @@ export const infoList = [
 export const toolsData = [
     assets.vscode, assets.firebase, assets.mongodb, assets.figma, assets.git, assets.gazebo, assets.quartus, assets.keil, assets.arduino, assets.ros2, assets.solidworks, assets.vivado
 ];
+
 
 
 
