9cb2ec7228fb178946c850b4fab6884119b71e0b
Matthew Garcia <127278603+Matthew-Garcia@users.noreply.github.com>
2026-01-17 16:43:02 -0600

    Update page.jsx

--- app/projects/nasa-lunabotics/page.jsx
+++ app/projects/nasa-lunabotics/page.jsx
@@ -122,7 +122,7 @@ export default function NasaLunaboticsPage() {
               initial={{ opacity: 0, y: 20 }}
               whileInView={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.6 }}
-              src="/lunabotics-rover-moving.webm"
+              src="/lunabotics-rover-moving.mp4"
               autoPlay
               loop
               muted
