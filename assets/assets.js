24121c4a5200f00e7415bdc9b20e718dbfd2f94b
Matthew Garcia <127278603+Matthew-Garcia@users.noreply.github.com>
2026-02-02 22:20:33 -0600

    Update assets.js

--- assets/assets.js
+++ assets/assets.js
@@ -1,163 +1,196 @@
-import user_image from './user-image.png';
-import code_icon from './code-icon.png';
-import code_icon_dark from './code-icon-dark.png';
-import edu_icon from './edu-icon.png';
-import edu_icon_dark from './edu-icon-dark.png';
-import project_icon from './project-icon.png';
-import project_icon_dark from './project-icon-dark.png';
-import vscode from './vscode.png';
-import firebase from './firebase.png';
-import figma from './figma.png';
-import git from './git.png';
-import mongodb from './mongodb.png';
-import right_arrow_white from './right-arrow-white.png';
-import logo from './logo.png';
-import logo_dark from './logo_dark.png';
-import mail_icon from './mail_icon.png';
-import mail_icon_dark from './mail_icon_dark.png';
-import profile_img from './profile-img.png';
-import download_icon from './download-icon.png';
-import hand_icon from './hand-icon.png';
-import header_bg_color from './header-bg-color.png';
-import moon_icon from './moon_icon.png';
-import sun_icon from './sun_icon.png';
-import arrow_icon from './arrow-icon.png';
-import arrow_icon_dark from './arrow-icon-dark.png';
-import menu_black from './menu-black.png';
-import menu_white from './menu-white.png';
-import close_black from './close-black.png';
-import close_white from './close-white.png';
-import web_icon from './web-icon.png';
-import mobile_icon from './mobile-icon.png';
-import ui_icon from './ui-icon.png';
-import graphics_icon from './graphics-icon.png';
-import right_arrow from './right-arrow.png';
-import send_icon from './send-icon.png';
-import right_arrow_bold from './right-arrow-bold.png';
-import right_arrow_bold_dark from './right-arrow-bold-dark.png';
-import gazebo from './gazebo.png';
-import quartus from './Quartus_prime_icon.png';
-import keil from './keil.png';
-import arduino from './arduino.png';
-import ros2 from './ros2.png';
-import solidworks from './solidworks.png';
-import vivado from './vivado.png';
+import user_image from "./user-image.png";
+import code_icon from "./code-icon.png";
+import code_icon_dark from "./code-icon-dark.png";
+import edu_icon from "./edu-icon.png";
+import edu_icon_dark from "./edu-icon-dark.png";
+import project_icon from "./project-icon.png";
+import project_icon_dark from "./project-icon-dark.png";
+import vscode from "./vscode.png";
+import firebase from "./firebase.png";
+import figma from "./figma.png";
+import git from "./git.png";
+import mongodb from "./mongodb.png";
+import right_arrow_white from "./right-arrow-white.png";
+import logo from "./logo.png";
+import logo_dark from "./logo_dark.png";
+import mail_icon from "./mail_icon.png";
+import mail_icon_dark from "./mail_icon_dark.png";
+import profile_img from "./profile-img.png";
+import download_icon from "./download-icon.png";
+import hand_icon from "./hand-icon.png";
+import header_bg_color from "./header-bg-color.png";
+import moon_icon from "./moon_icon.png";
+import sun_icon from "./sun_icon.png";
+import arrow_icon from "./arrow-icon.png";
+import arrow_icon_dark from "./arrow-icon-dark.png";
+import menu_black from "./menu-black.png";
+import menu_white from "./menu-white.png";
+import close_black from "./close-black.png";
+import close_white from "./close-white.png";
+import web_icon from "./web-icon.png";
+import mobile_icon from "./mobile-icon.png";
+import ui_icon from "./ui-icon.png";
+import graphics_icon from "./graphics-icon.png";
+import right_arrow from "./right-arrow.png";
+import send_icon from "./send-icon.png";
+import right_arrow_bold from "./right-arrow-bold.png";
+import right_arrow_bold_dark from "./right-arrow-bold-dark.png";
+import gazebo from "./gazebo.png";
+import quartus from "./Quartus_prime_icon.png";
+import keil from "./keil.png";
+import arduino from "./arduino.png";
+import ros2 from "./ros2.png";
+import solidworks from "./solidworks.png";
+import vivado from "./vivado.png";
 import lunabotics_rover from "./lunabotics-rover.png";
 
-
-
-
-
-
 export const assets = {
-    user_image,
-    code_icon,
-    code_icon_dark,
-    edu_icon,
-    edu_icon_dark,
-    project_icon,
-    project_icon_dark,
-    vscode,
-    firebase,
-    figma,
-    git,
-    mongodb,
-    right_arrow_white,
-    logo,
-    logo_dark,
-    mail_icon,
-    mail_icon_dark,
-    profile_img,
-    download_icon,
-    hand_icon,
-    header_bg_color,
-    moon_icon,
-    sun_icon,
-    arrow_icon,
-    arrow_icon_dark,
-    menu_black,
-    menu_white,
-    close_black,
-    close_white,
-    web_icon,
-    mobile_icon,
-    ui_icon,
-    graphics_icon,
-    right_arrow,
-    send_icon,
-    right_arrow_bold,
-    right_arrow_bold_dark,
-    gazebo,
-    quartus,
-    keil,
-    arduino,
-    ros2,
-    solidworks,
-    vivado,
-    lunabotics_rover,
-    
+  user_image,
+  code_icon,
+  code_icon_dark,
+  edu_icon,
+  edu_icon_dark,
+  project_icon,
+  project_icon_dark,
+  vscode,
+  firebase,
+  figma,
+  git,
+  mongodb,
+  right_arrow_white,
+  logo,
+  logo_dark,
+  mail_icon,
+  mail_icon_dark,
+  profile_img,
+  download_icon,
+  hand_icon,
+  header_bg_color,
+  moon_icon,
+  sun_icon,
+  arrow_icon,
+  arrow_icon_dark,
+  menu_black,
+  menu_white,
+  close_black,
+  close_white,
+  web_icon,
+  mobile_icon,
+  ui_icon,
+  graphics_icon,
+  right_arrow,
+  send_icon,
+  right_arrow_bold,
+  right_arrow_bold_dark,
+  gazebo,
+  quartus,
+  keil,
+  arduino,
+  ros2,
+  solidworks,
+  vivado,
+  lunabotics_rover,
 };
 
 export const workData = [
-    
-    {
-      title: "NASA Lunabotics Autonomous Rover",
-      description: "Embedded Systems, ROS 2, Autonomous Robotics",
-      bgImage: '/lunabotics-rover.png',
-      link: "/projects/nasa-lunabotics",
-    },
-    {
-        title: 'SCARA Robot Arm',
-        description: 'Embedded Systems & Robotics',
-        bgImage: '/SCARA-Robot-Arm.gif',
-        link: '/projects/scara-robot'
-    },
-    {
-        title: 'Smart IoT Pet Feeder',
-        description: 'IoT & Mobile App Integration',
-        bgImage: '/Smart_Bowl.png',
-    },
-    
-    {
-        title: 'LiDAR-Based Mobile Robot',
-        description: 'Autonomous Navigation with ROS2',
-        bgImage: '/work-4.png',
-    },
-]
+  {
+    title: "NASA Lunabotics Autonomous Rover",
+    description: "Embedded Systems, ROS 2, Autonomous Robotics",
+    bgImage: "/lunabotics-rover.png",
+    link: "/projects/nasa-lunabotics",
+  },
+  {
+    title: "SCARA Robot Arm",
+    description: "Embedded Systems & Robotics",
+    bgImage: "/SCARA-Robot-Arm.gif",
+    link: "/projects/scara-robot",
+  },
+  {
+    title: "Smart IoT Pet Feeder",
+    description: "IoT & Mobile App Integration",
+    bgImage: "/Smart_Bowl.png",
+  },
+
+  {
+    title: "LPC4088 Embedded Firmware",
+    description: "Bare-Metal, I2C Drivers, ARM Assembly",
+    bgImage: "/lpc4088-board.png",
+    link: "/projects/lpc4088-embedded-firmware",
+  },
+
+  {
+    title: "LiDAR-Based Mobile Robot",
+    description: "Autonomous Navigation with ROS2",
+    bgImage: "/work-4.png",
+  },
+];
 
 export const serviceData = [
-    { icon: assets.web_icon, title: 'Embedded Systems Development', description: 'Designing real-time embedded applications using C/C++, microcontrollers (e.g., Arduino, STM32), and RTOS...', link: 'https://github.com/Matthew-Garcia/SCARA-Robot' },
-    { icon: assets.mobile_icon, title: 'Robotics & Automation', description: 'Building autonomous robotic systems with ROS2, motion planning, PID control, and sensor fusion...', link: '' },
-    { icon: assets.ui_icon, title: 'FPGA & Digital Logic Design', description: 'Developing hardware logic (VHDL/Verilog) for ALUs, controllers, and signal processing systems...', link: '' },
-    { icon: assets.graphics_icon, title: 'IoT & Edge Computing', description: 'Connecting devices using Wi-Fi/Bluetooth and processing data locally on microcontrollers or SBCs....', link: '' },
-]
+  {
+    icon: assets.web_icon,
+    title: "Embedded Systems Development",
+    description:
+      "Designing real-time embedded applications using C/C++, microcontrollers (e.g., Arduino, STM32), and RTOS...",
+    link: "https://github.com/Matthew-Garcia/SCARA-Robot",
+  },
+  {
+    icon: assets.mobile_icon,
+    title: "Robotics & Automation",
+    description:
+      "Building autonomous robotic systems with ROS2, motion planning, PID control, and sensor fusion...",
+    link: "",
+  },
+  {
+    icon: assets.ui_icon,
+    title: "FPGA & Digital Logic Design",
+    description:
+      "Developing hardware logic (VHDL/Verilog) for ALUs, controllers, and signal processing systems...",
+    link: "",
+  },
+  {
+    icon: assets.graphics_icon,
+    title: "IoT & Edge Computing",
+    description:
+      "Connecting devices using Wi-Fi/Bluetooth and processing data locally on microcontrollers or SBCs....",
+    link: "",
+  },
+];
 
 export const infoList = [
-    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'C/C++, Python, JavaScript, Verilog, VHDL, ARM Assembly, Bash, HTML/CSS, ROS2, SQL' },
-    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'B.S. in Computer Engineering (May 2026), A.S. in Pre-Engineering, A.A. in General Studies' },
-    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Developed 10+ projects in robotics, embedded systems, and hardware–software integration'  }
+  {
+    icon: assets.code_icon,
+    iconDark: assets.code_icon_dark,
+    title: "Languages",
+    description:
+      "C/C++, Python, JavaScript, Verilog, VHDL, ARM Assembly, Bash, HTML/CSS, ROS2, SQL",
+  },
+  {
+    icon: assets.edu_icon,
+    iconDark: assets.edu_icon_dark,
+    title: "Education",
+    description:
+      "B.S. in Computer Engineering (May 2026), A.S. in Pre-Engineering, A.A. in General Studies",
+  },
+  {
+    icon: assets.project_icon,
+    iconDark: assets.project_icon_dark,
+    title: "Projects",
+    description:
+      "Developed 10+ projects in robotics, embedded systems, and hardware–software integration",
+  },
 ];
 
 export const toolsData = [
-    assets.vscode, assets.firebase, assets.mongodb, assets.figma, assets.git, assets.gazebo, assets.quartus, assets.keil, assets.arduino, assets.ros2, assets.solidworks, assets.vivado
+  assets.vscode,
+  assets.firebase,
+  assets.mongodb,
+  assets.figma,
+  assets.git,
+  assets.gazebo,
+  assets.quartus,
+  assets.keil,
+  assets.arduino,
+  assets.ros2,
+  assets.solidworks,
+  assets.vivado,
 ];
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
