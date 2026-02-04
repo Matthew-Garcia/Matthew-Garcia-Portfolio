dcf43aad62b08e2f39db120934ec3b2040e42c74
Matthew Garcia <127278603+Matthew-Garcia@users.noreply.github.com>
2026-02-03 23:42:16 -0600

    Update Work.jsx

--- components/Work.jsx
+++ components/Work.jsx
@@ -59,7 +59,7 @@ const Work = ({ isDarkMode }) => {
               className="aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group w-full"
               style={{ backgroundImage: `url(${project.bgImage})` }}
             >
-              <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
+              <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-2 px-4 flex items-center justify-between duration-500 group-hover:bottom-7">
                 <div>
                   <h2 className="font-semibold">{project.title}</h2>
                   <p className="text-sm text-gray-700">{project.description}</p>
@@ -142,3 +142,4 @@ const Work = ({ isDarkMode }) => {
 };
 
 export default Work;
+
